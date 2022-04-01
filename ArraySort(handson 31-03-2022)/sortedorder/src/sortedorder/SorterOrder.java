27f214c24e3f0082ab4cfed3df86ce66f938477d
muthu <95739546+muthukamatchip@users.noreply.github.com>
2022-04-01 11:08:41 +0530

    Add files via upload

---
+++ ArraySort(handson 31-03-2022)/sortedorder/src/sortedorder/SorterOrder.java
@@ -0,0 +1,23 @@
+package sortedorder;
+
+import java.util.Arrays;
+
+public class SorterOrder 
+{
+
+	public static void main(String[] args)
+{
+	 int[] arrays=new int [] {2,34,5,1,84,18,98,56,23};
+       Arrays.sort(arrays);
+       
+       System.out.println("sorter");
+  
+     for (int i=0;i<arrays.length;i++)
+     {
+       System.out.println(arrays[i]);
+     }
+    	
+}
+}
+
+	
