a692bc6b9af1f021b203c1a23c0dbbf948e0b209
muthu <95739546+muthukamatchip@users.noreply.github.com>
2022-04-01 11:53:22 +0530

    Add files via upload

---
+++ PracticeCode(handson 31-03-2022)/sortedorder/src/sortedorder/j.java
@@ -0,0 +1,24 @@
+package sortedorder;
+
+import java.util.Arrays;
+
+public class j {
+
+	public static void main(String[] args) {
+
+		 int[] arrays=new int [] {2,34,5,1,84,18,98,56,23};
+	       Arrays.sort(arrays);
+	       
+	       System.out.println("sorter");
+	  
+	     for (int i=0;i<arrays.length;i++)
+	     {
+	       System.out.println(arrays[i]);
+	     }
+	    	
+	}
+	
+
+	}
+
+
