91c3136d64bec14c88de2e4537e265813892bf96
muthu <95739546+muthukamatchip@users.noreply.github.com>
2022-04-07 14:49:24 +0530

    Add files via upload

---
+++ PracticeCode(07-04-2022)/JavaHashMap.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class JavaHashMap {
+
+public void student()
+{
+	HashMap <String,Integer> s =new HashMap<>();
+	s.put("Dinesh",107);
+	s.put("Mahesh",108);
+	s.put("Divya",109);
+	s.put("Suresh",110);
+	System.out.println(s);
+}
+	public static void main(String[] args) {
+		JavaHashMap hm =new JavaHashMap();
+		hm.student();
+		
+	}
+
+}
